2659fc554e7c559b29f44c41691eb3d34bf08615
Nicole L Limtiaco <limni@plus.seas.upenn.edu>
2014-04-02 14:04:34 -0400

    touched up results view layout

--- CampusMap/src/upenn/cis350/campusmap/Views/ResultsEntryView.java
+++ CampusMap/src/upenn/cis350/campusmap/Views/ResultsEntryView.java
@@ -1,14 +1,19 @@
 package upenn.cis350.campusmap.Views;
 
+import android.R.color;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Typeface;
 import android.util.EventLog.Event;
 import android.view.MotionEvent;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 
 public class ResultsEntryView extends TextView{
+	private LinearLayout layout;
 	private TextView nameView;
 	private TextView addressView;
 	
@@ -21,12 +26,24 @@ public class ResultsEntryView extends TextView{
 		nameView.setPadding(20,10,0,0);
 		nameView.setTextSize(24);
 		nameView.setTypeface(Typeface.SANS_SERIF);
+		nameView.setTextColor(555);
+		nameView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
 		
 		//addressView
 		addressView.setText("address");
-		addressView.setPadding(20, 0, 0, 0);
+		addressView.setPadding(20, 0, 0, 10);
 		addressView.setTextSize(16);
 		addressView.setTypeface(Typeface.SANS_SERIF);
+		addressView.setTextColor(color.darker_gray);
+		addressView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+		
+		//set up layout
+		layout = new LinearLayout(context);
+		layout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+		layout.setOrientation(LinearLayout.VERTICAL);
+		layout.addView(nameView);
+		layout.addView(addressView);
+		
 	}
 	
 	@Override
--- CampusMap/src/upenn/cis350/campusmap/Views/ResultsView.java
+++ CampusMap/src/upenn/cis350/campusmap/Views/ResultsView.java
@@ -3,30 +3,49 @@ package upenn.cis350.campusmap.Views;
 import java.util.ArrayList;
 import java.util.List;
 
+
+import upenn.cis350.campusmap.R;
+import android.R.color;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RectShape;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ResultsView extends View{
-	List<ResultsEntryView> results = new ArrayList<ResultsEntryView>();
+	LinearLayout layout;
 	public ResultsView(Context context, Bundle b) {
 		super(context);
+		//setup outer vertical linear layout
+		layout = new LinearLayout(context);
+		layout.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+		layout.setOrientation(LinearLayout.VERTICAL);
+		
+		//divider line between entries
+		ShapeDrawable divider = new ShapeDrawable(new RectShape());
+		divider.getPaint().setColor(color.darker_gray);
+		divider.setBounds(10, 0, 200, 3);
+		layout.setDividerDrawable(getResources().getDrawable(R.drawable.divider));
+		layout.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE);
+		//get entry results
 		List<String> names = b.getStringArrayList("names");
 		List<String> addresses = b.getStringArrayList("addresses");
 		for(int i = 0; i < names.size(); i++){
 			ResultsEntryView result = new ResultsEntryView(context, names.get(i), addresses.get(i));
-			results.add(result);
+			layout.addView(result);
 		}
+		
 	}
 	
 	public void onDraw(Canvas canvas){
-		for(ResultsEntryView r : results){
-			r.draw(canvas);
-		}
+		layout.draw(canvas);
 	}
 
 
