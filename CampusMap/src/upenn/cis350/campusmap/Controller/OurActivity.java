4343f427aaf449b7e2de6e07150b00d250c4fe3f
dhrupadb <dhrupadb@seas.upenn.edu>
2014-05-05 05:53:10 -0400

    OurActivity

---
+++ CampusMap/src/upenn/cis350/campusmap/Controller/OurActivity.java
@@ -0,0 +1,11 @@
+package upenn.cis350.campusmap.Controller;
+
+import java.util.List;
+
+import android.app.Activity;
+
+public class OurActivity extends Activity{
+	public void receiveSearchResults(List<Building> results) {
+		
+	}
+}
