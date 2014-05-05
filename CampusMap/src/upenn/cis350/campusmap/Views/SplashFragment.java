dd94340c0615063c0703c963988fe1ffa0f0cfb1
dhrupadb <dhrupadb@seas.upenn.edu>
2014-05-05 00:59:40 -0400

    Splash Activity and gray icon

---
+++ CampusMap/src/upenn/cis350/campusmap/Views/SplashFragment.java
@@ -0,0 +1,42 @@
+package upenn.cis350.campusmap.Views;
+
+import upenn.cis350.campusmap.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SplashFragment extends Fragment {
+
+    private TextView skipLoginButton;
+    private SkipLoginCallback skipLoginCallback;
+
+    public interface SkipLoginCallback {
+        void onSkipLoginPressed();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.loading, container, false);
+
+        skipLoginButton = (TextView) view.findViewById(R.id.skip_login_button);
+        skipLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (skipLoginCallback != null) {
+                    skipLoginCallback.onSkipLoginPressed();
+                }
+            }
+        });
+
+        return view;
+    }
+
+    public void setSkipLoginCallback(SkipLoginCallback callback) {
+        skipLoginCallback = callback;
+    }
+}
