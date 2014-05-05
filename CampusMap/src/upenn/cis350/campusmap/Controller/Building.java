167215751bcca7c2387d44708dffac3c1ad1d287
Dhrupad Bhardwaj <dhrupadb@seas.upenn.edu>
2014-05-05 10:15:10 -0400

    Update Building.java

--- CampusMap/src/upenn/cis350/campusmap/Controller/Building.java
+++ CampusMap/src/upenn/cis350/campusmap/Controller/Building.java
@@ -8,6 +8,8 @@ import java.util.Date;
 
 import org.apache.http.ParseException;
 
+import android.util.Log;
+
 public class Building {
 	private double longitude;
 	private double latitude;
@@ -22,6 +24,7 @@ public class Building {
 	private String weekend_hours;
 	private String description;
 	private Hours h;
+	private String origName;
 
 	public Building(double longitude, double latitude, String id, String icon,
 			String name, String address) {
@@ -30,6 +33,7 @@ public class Building {
 		this.googlePlaceID = id;
 		this.iconURL = icon;
 		this.name = name;
+		this.origName = name;
 		this.address = address;
 		this.nicknames = new ArrayList<String>();
 		this.reg_hours = "";
@@ -37,6 +41,10 @@ public class Building {
 		this.description = "";
 	}
 
+	public void revertName() {
+		this.name = this.origName;
+	}
+	
 	class Hours {
 		// internal class that checks if the building is open based on
 		// the building's passed in hours
@@ -99,22 +107,22 @@ public class Building {
 			if (regOpen == null)
 				return false;
 			boolean op = false;
-			// //System.out.println("got to main function");
+			// //Log.v("from printing building","got to main function");
 			int day = current.get(Calendar.DAY_OF_WEEK);
 			if (day != 0 && day != 7) {
-				// //System.out.println("got the correct day of the week");
+				// //Log.v("from printing building","got the correct day of the week");
 				boolean before = current.get(Calendar.HOUR_OF_DAY) < regClose
 						.get(Calendar.HOUR_OF_DAY);
 				before = before
 						& (current.get(Calendar.MINUTE) < regClose
 								.get(Calendar.MINUTE));
-				// System.out.println("close hour: " +
+				// Log.v("from printing building","close hour: " +
 				// regClose.get(Calendar.HOUR_OF_DAY));
-				// System.out.println("current hours: " +
+				// Log.v("from printing building","current hours: " +
 				// current.get(Calendar.HOUR_OF_DAY));
 				boolean after = current.after(regOpen);
-				// System.out.println("before bool: " + before);
-				// System.out.println("after bool: " + after);
+				// Log.v("from printing building","before bool: " + before);
+				// Log.v("from printing building","after bool: " + after);
 				op = (before & after);
 				return op;
 			}
@@ -131,16 +139,15 @@ public class Building {
 
 	public void printBuilding() {
 		// prints out the building information for testing purposes
-		System.out.println("Name: " + name);
-		System.out.println("Address: " + address);
-		System.out.println("Longitude: " + longitude + " and Latitude: "
+		Log.v("from printing building","Name: " + name);
+		Log.v("from printing building","Address: " + address);
+		Log.v("from printing building","Longitude: " + longitude + " and Latitude: "
 				+ latitude);
 		System.out.print("Nicknames: ");
 		for (String x : this.nicknames)
 			System.out.print(x + ", ");
-		System.out.println();
-		System.out.println("reg hours: " + reg_hours);
-		System.out.println("weekend hours: " + weekend_hours);
+		Log.v("from printing building","reg hours: " + reg_hours);
+		Log.v("from printing building","weekend hours: " + weekend_hours);
 
 	}
 
@@ -149,6 +156,13 @@ public class Building {
 		return h.isOpen();
 	}
 
+	
+	public boolean equalTo (Building b) {
+		boolean lat = b.getLatitude() == this.getLatitude();
+		boolean lon = b.getLongitude() == this.getLongitude();
+		return lat && lon;
+	}
+	
 	public void addNicknames(String n) {
 		if (n == null)
 			return;
