ae77949241eaf3a924a92144cfb2782ab75d05dd
Max Morant <mdmorant@gmail.com>
2014-05-05 01:35:42 -0400

    GPS

--- CampusMap/src/upenn/cis350/campusmap/Controller/GPSTracker.java
+++ CampusMap/src/upenn/cis350/campusmap/Controller/GPSTracker.java
@@ -29,12 +29,11 @@ public final class GPSTracker implements LocationListener {
 	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1; // 10 meters
 
 	// The minimum time between updates in milliseconds
-	private static final long MIN_TIME_BW_UPDATES = 1; // 1 minute
+	private static final long MIN_TIME_BW_UPDATES = 10000; // 1 minute
 
 	// Declaring a Location Manager
 	protected LocationManager locationManager;
 	
-	private static long time = System.currentTimeMillis();
 
 	public GPSTracker(Context context) {
 		this.mContext = context;
@@ -48,59 +47,58 @@ public final class GPSTracker implements LocationListener {
 	 */
 	public Location getLocation() {
 		try {
-			if (System.currentTimeMillis() - time > 10000) {
-				time = System.currentTimeMillis();
-				
-			}
+		
 			locationManager = (LocationManager) mContext
 					.getSystemService(Context.LOCATION_SERVICE);
-			Criteria crit = new Criteria();
-			String provider = locationManager.getBestProvider(crit, true);
-			location = locationManager.getLastKnownLocation(provider);
-//			// getting GPS status
-//			isGPSEnabled = locationManager
-//					.isProviderEnabled(LocationManager.GPS_PROVIDER);
-//
-//			Log.v("isGPSEnabled", "=" + isGPSEnabled);
-//
-//			// getting network status
-//			isNetworkEnabled = locationManager
-//					.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-//
-//			Log.v("isNetworkEnabled", "=" + isNetworkEnabled);
-//
-//			if (isGPSEnabled == false && isNetworkEnabled == false) {
-//				// no network provider is enabled
-//			} else {
-//				this.canGetLocation = true;
-//				if (isNetworkEnabled) {
-//					location = null;
-//					locationManager.requestLocationUpdates(
-//							LocationManager.NETWORK_PROVIDER,
-//							MIN_TIME_BW_UPDATES,
-//							MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-//					Log.d("Network", "Network");
-//					if (locationManager != null) {
-//						location = locationManager
-//								.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-//					}
-//				}
-//				// if GPS Enabled get lat/long using GPS Services
-//				if (isGPSEnabled) {
-//					location = null;
-//					if (location == null) {
-//						locationManager.requestLocationUpdates(
-//								LocationManager.GPS_PROVIDER,
-//								MIN_TIME_BW_UPDATES,
-//								MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-//						Log.d("GPS Enabled", "GPS Enabled");
-//						if (locationManager != null) {
-//							location = locationManager
-//									.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-//						}
-//					}
-//				}
-//			}
+//			Criteria crit = new Criteria();
+//			String provider = locationManager.getBestProvider(crit, true);
+//			location = locationManager.getLastKnownLocation(provider);
+			// getting GPS status
+			isGPSEnabled = locationManager
+					.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+			Log.v("isGPSEnabled", "=" + isGPSEnabled);
+
+			// getting network status
+			isNetworkEnabled = locationManager
+					.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+			Log.v("isNetworkEnabled", "=" + isNetworkEnabled);
+
+			if (isGPSEnabled == false && isNetworkEnabled == false) {
+				// no network provider is enabled
+			} else {
+				this.canGetLocation = true;
+				if (isNetworkEnabled) {
+					location = null;
+					locationManager.requestLocationUpdates(
+							LocationManager.NETWORK_PROVIDER,
+							MIN_TIME_BW_UPDATES,
+							MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+					Log.d("Network", "Network");
+					if (locationManager != null) {
+						locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, this.MIN_TIME_BW_UPDATES, this.MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+						location = locationManager
+								.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+					}
+				}
+				// if GPS Enabled get lat/long using GPS Services
+				if (isGPSEnabled) {
+					location = null;
+					if (location == null) {
+						locationManager.requestLocationUpdates(
+								LocationManager.GPS_PROVIDER,
+								MIN_TIME_BW_UPDATES,
+								MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+						Log.d("GPS Enabled", "GPS Enabled");
+						if (locationManager != null) {
+							locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, this.MIN_TIME_BW_UPDATES, this.MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+							location = locationManager
+									.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+						}
+					}
+				}
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
